a6f91e714b928c63faf6c496c9e70041e6d10d88
joyousharon <joyousharon@gmail.com>
2024-11-21 14:33:44 +0800

    Site updated: 2024-11-21 14:33:42

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/11/20/我的sxm博客/","2024/11/20/hello-world/"];function toRandomPost(){
+var posts=["2024/11/21/唯有清风笑/","2024/11/20/我的sxm博客/","2024/11/20/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
