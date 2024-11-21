03600e56914129866641b250b0983527fac12df3
joyousharon <joyousharon@gmail.com>
2024-11-21 14:21:11 +0800

    Site updated: 2024-11-21 14:21:10

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/11/20/hello-world/","2024/11/20/我的sxm博客/"];function toRandomPost(){
+var posts=["2024/11/20/我的sxm博客/","2024/11/20/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
