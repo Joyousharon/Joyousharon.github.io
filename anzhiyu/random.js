32d2936769584b678e00b54162b7133cddd6d216
joyousharon <joyousharon@gmail.com>
2024-11-26 09:59:03 +0800

    Site updated: 2024-11-26 09:59:01

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["undefined/她的梦里/","undefined/halo搭建脚本/","undefined/唯有清风笑/","undefined/我的sxm博客/","undefined/hello-world/"];function toRandomPost(){
+var posts=["undefined/halo搭建脚本/","undefined/她的梦里/","undefined/唯有清风笑/","undefined/我的sxm博客/","undefined/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
