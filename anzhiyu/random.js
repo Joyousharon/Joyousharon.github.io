f23be0b8934444d642cd50c864d303c0d12f4b53
joyousharon <joyousharon@gmail.com>
2024-12-01 23:00:45 +0800

    Site updated: 2024-12-01 23:00:42

---
+++ anzhiyu/random.js
@@ -0,0 +1,3 @@
+var posts=["undefined/按摩/","undefined/她的梦/","undefined/halo搭建脚本/","undefined/她的梦里/","undefined/何以缘灭凰归离/","undefined/你走以后/","undefined/何以缘起凤相思/","undefined/小诺和小雨/","undefined/小萝莉/","undefined/我是谁/"];function toRandomPost(){
+    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
+  };
